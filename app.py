1191070ce9882e76c17db6a11dec90c27db23dcc
arn <253449@it2id.upchiapas.edu.mx>
2026-04-17 20:46:08 -0600

    Initial commit

---
+++ app.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+st.set_page_config(page_title="Mi App de Estadística", page_icon="📊")
+
+st.title("📊 Mi Primera App de Estadística")
+st.write("¡Si estás viendo esto en tu navegador, todo está instalado correctamente y estamos listos para empezar!")
